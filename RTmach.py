dea19cc0ce447fcd31697f1e467583433cf45f37
Xin Bian <bianxin2006@gmail.com>
2017-10-26 20:51:56 -0400

    remove some bugs

--- RTmach.py
+++ RTmach.py
@@ -13,13 +13,15 @@ import numpy as np
 import pylab
 import matplotlib.pyplot as plt
 
-istep = '000010'
-Lz=3.2
-Ly=0.4
+istep = '000005'
+Lz=0.08
+Ly=0.01
 g = 1.0
 waveLen = Ly
 variable = ['PVx','PVy','PVz','PPress', 'Prho']
-h5file = h5py.File(filepath,'r')
+gamma=1.66667
+delimiter =''
+h5file = h5py.File('tests_single_new.h5','r')
 #read dataset dimensions
 mylist = ['Fields/','Prho','/',istep]
 filepath = delimiter.join(mylist)
@@ -31,33 +33,33 @@ nx=m1.shape[2]
 dz=Lz/nz
 
 #inteface postion wrt grid points
-z0 = nz/2
-machGiven = 0.4
+z0 = 10*nz/16
+machGiven = 1.2
 #define in spike line
 #density at interface
 rho = (m1[z0, ny/2-1, 0] + m1[z0-1, ny/2-1, 0])/2
-
+rho = 0.8
 #pressure  
 mylist = ['Fields/','PPress','/',istep]
 filepath = delimiter.join(mylist)
 databk = h5file.get(filepath)
 m1 = np.array(databk)
 press = (m1[z0, ny/2-1, 0] + m1[z0-1, ny/2-1, 0])/2
-
+pressTop = m1[nz-1,ny/2-1,0]
 wave = (g*waveLen)**0.5
-cs = (press/rho)**0.5
+cs = (gamma*press/rho)**0.5
 mach = wave/cs
 print 'mach number at interface is:', mach
 
 
 
 #####given mach number calculate pressure needed
-pressNeed = rho*g*waveLen/machGiven**2
+pressNeed = rho*g*waveLen/(gamma*machGiven**2)
 print 'the pressure and density at interface is:', press, rho
 print 'for Mach=', machGiven, "the pressure needed is:", pressNeed
-print 'you need add to the pressure intial constant:' pressNeed - press
+print 'you need add to the pressure intial constant:', pressNeed - press
 
-
+print 'pressure at top is:', pressTop
 
 
 
