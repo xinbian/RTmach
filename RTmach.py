92047c0ef7249db912b9b2939c070fa3c8f962dd
Xin Bian <xinbian@users.noreply.github.com>
2018-02-08 13:53:19 -0500

    add omega calc

--- RTmach.py
+++ RTmach.py
@@ -75,6 +75,37 @@ for istep in step:
 	filepath = delimiter.join(mylist)
 	h5new.create_dataset(filepath,data=div)
 	
+	#omega 
+	
+	dyVz = np.gradient(vz, dy, axis=1)
+	dzVy = np.gradient(vy, dz, axis=0)
+	
+	omega_x = dyVz - dzVy
+	
+	mylist = ['Fields/','POmex','/',istep]
+	filepath = delimiter.join(mylist)
+	h5new.create_dataset(filepath,data=omega_x)
+	
+	
+	if nx != 1:
+		dyVx = np.gradient(vx, dy, axis=1)
+		dzVx = np.gradient(vx, dz, axis=0)
+		dxVy = np.gradient(vy, dx, axis=2)
+		dxVz = np.gradient(vz, dx, axis=2)
+		
+		omega_y = dzVx - dxVz
+		omega_z = dxVy - dyVx
+		
+		mylist = ['Fields/','POmey','/',istep]
+		filepath = delimiter.join(mylist)
+		h5new.create_dataset(filepath,data=omega_y)
+		
+		mylist = ['Fields/','POmez','/',istep]
+		filepath = delimiter.join(mylist)
+		h5new.create_dataset(filepath,data=omega_z)
+	
+	
+	
 	print 'progress:', float(istep)/totalsteps*100, '%'
 
 
